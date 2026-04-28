a5cc12f3c344e70677dd17c1e2a542168f6027ae
wambuaraphael804-maker <wambuaraphael804@gmail.com>
2026-04-28 20:29:41 +0000

    fix cors syntax

--- server.js
+++ server.js
@@ -19,7 +19,13 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    "https://v0-website-deployment-sage.vercel.app",
+    "http://localhost:3000"
+  ],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -62,4 +68,8 @@ process.on("SIGTERM", () => {
   server.close(() => {
     console.log("HTTP server closed");
   });
+});
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
 });
